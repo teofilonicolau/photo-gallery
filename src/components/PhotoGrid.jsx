86df36690528f2519abc295ff8843d345e246833
TEOFILO NICOLAU <teofilonicolau157@gmail.com>
2025-01-06 20:30:07 -0300

    nelhorias

--- src/components/PhotoGrid.jsx
+++ src/components/PhotoGrid.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import PhotoCard from './PhotoCard';
 
 const PhotoGrid = ({ photos }) => {
+  if (photos.length === 0) {
+    return <p>Nenhuma foto encontrada</p>;
+  }
+
   return (
     <div className="photo-grid">
       {photos.map((photo) => (
